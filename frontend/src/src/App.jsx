ff50879d66834e0a433545fc29743ea088d69382
Stephen Walsh <sgawalsh@gmail.com>
2026-06-06 18:06:45 -0400

    Remove unused components and swears

--- frontend/src/src/App.jsx
+++ frontend/src/src/App.jsx
@@ -2,14 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import JobForm from './pages/jobForm';
 import SessionStatus from './pages/sessionStatus';
 import NotFound from './pages/notFound';
-import ResultRouter from './pages/results/resultRouter';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<JobForm />} />
       <Route path="/sessions/:id" element={<SessionStatus />} />
-      <Route path="/results/:type/:id" element={<ResultRouter />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- frontend/src/src/pages/results/genericResult.jsx
+++ frontend/src/src/pages/results/genericResult.jsx
@@ -1,4 +1,4 @@
 function GenericResult({ results }) {
-  return (<p>IDK I'm a fucking idiot</p>);
+  return (<p>Whoops something has gone horribly and terribly wrong.</p>);
 }
 export default GenericResult;
